94453f1bbf66e1bfb2c6bda48eed473a96f9498f
mikan <santanud5d80@gmail.com>
2022-06-23 21:15:08 +0530

    footer added

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.600 "
+    borderTop="1px"
+    borderColor="gray.100"
+  >
+    2021 Realter,Inc.
+    <Text> made with ❤️ by mikan</Text>
+  </Box>
+);
+
+export default Footer;
